93b33e2b4ac4d3f4bdad061e83d7d49df2781684
leighiam.virrey <virreyleighiam@gmail.com>
2022-03-08 09:47:57 -0700

    Finished

---
+++ Install_files.ps1
@@ -0,0 +1,47 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function Input_Windows_Key {
+    Set-Location $PSScriptRoot
+    windows_key = (Get-Content .\key.txt)
+    Set-WindowsProductKey -Path "C:\Windows" -ProductKey windows_key
+}
+
+
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+
+if ((Get-WMIObject win32_operatingsystem).name -like "*Education*") {
+    Input_Windows_Key
+    if ($?){
+        Write-Host("Windows key has been applied") -ForegroundColor Green
+    }
+    else {
+        Write-Host("Something went wrong and the key has not been applied...") -ForegroundColor Red
+    }
+}
+
+$current_location = $PSScriptRoot
+$program_location = $current_location + "\Programs"
+$software_list = Get-ChildItem -Path $program_location
+$counter = 0
+foreach($software in $software_list){
+    Write-Host("$software is installing....")
+    Start-Process $software -ArgumentList "/passive" -Wait -WorkingDirectory $program_location
+    $counter++
+}
+Set-ExecutionPolicy Restricted
+Write-Host("Number of Software Installed: $counter")
+Pause
